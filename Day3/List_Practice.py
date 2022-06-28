835d4edb3180092ec6061657424985e2c9e48aa7
CtrlF4 <leew99@googlemail.com>
2022-06-28 20:51:00 +0100

    Added the first python file for this session

---
+++ Day3/List_Practice.py
@@ -0,0 +1,21 @@
+#-----------------------------------------------------------------
+# Python Lists
+# Date: 28/06/22
+#-----------------------------------------------------------------
+
+heights = [61, 70, 67, 64]
+
+# Note lists can mix datatypes
+ints_and_strings = [1, 2, 3, "four", "five"]
+
+mixed_list_common = ["Mia", 27, False, 0.5]
+sam_height_and_testscore = ["Sam", 67, 85.5, True]
+
+# Lists can also be empty
+my_empty_list = []
+
+#Lists also have methods
+append_example = ["This", "is", "an", "Example"]
+append_example.append("list")
+
+print(append_example)
