1730c2515886e1c807e81593348d7679137d6b77
CtrlF4 <leew99@googlemail.com>
2022-07-03 17:21:21 +0100

    prepped for tomorrow

---
+++ Day7/Medical_Insurance_Project_Lists.py
@@ -0,0 +1,30 @@
+#----------------------------------------------------------------------------------
+# This is the code for the Python Lists: Medical Insurance Estimation Project on the 
+# DATA SCIENTIST: ANALYTICS SPECIALIST career path on Codeacademy
+#
+# Date: 03/07/22
+#----------------------------------------------------------------------------------
+
+# In this project, you will examine how factors such as age, sex, BMI, number of children, and smoking status contribute to medical insurance costs.
+
+# You will apply your new knowledge of Python Lists to store insurance cost data in a list as well as compare estimated insurance costs to actual insurance costs.
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+# Function to estimate insurance cost:
+def estimate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
+  estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+  print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.")
+  return estimated_cost
+ 
+# Estimate Maria's insurance cost
+maria_insurance_cost = estimate_insurance_cost(name = "Maria", age = 31, sex = 0, bmi = 23.1, num_of_children = 1, smoker = 0)
+
+# Estimate Rohan's insurance cost
+rohan_insurance_cost = estimate_insurance_cost(name = 
+"Rohan", age = 25, sex = 1, bmi = 28.5, num_of_children = 3, smoker = 0)
+
+# Estimate Valentina's insurance cost
+valentina_insurance_cost = estimate_insurance_cost(name = "Valentina", age = 53, sex = 0, bmi = 31.4, num_of_children = 0, smoker = 1)
+
+# Add your code here
